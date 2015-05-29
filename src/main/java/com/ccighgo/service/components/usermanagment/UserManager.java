e679095af90527f00d23da05433cb63b8f87e9f3
Ravi Mishra <ravimishra.blr@gmail.com>
2015-05-29 16:12:05 -0500

    CCI_GH_GO, missing xsds

--- src/main/java/com/ccighgo/service/components/usermanagment/UserManager.java
+++ src/main/java/com/ccighgo/service/components/usermanagment/UserManager.java
@@ -3,7 +3,12 @@
  */
 package com.ccighgo.service.components.usermanagment;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Component;
+
+import com.ccighgo.exception.ErrorCode;
+import com.ccighgo.exception.ValidationException;
 
 /**
  * UserManager class servers as business component for user login and logout 
@@ -14,5 +19,18 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class UserManager {
+	
+	
+	public void validateRequired(String name, String value) throws ValidationException {
+		if(value == null || value.trim().equals("")){
+			throw new ValidationException(ErrorCode.MISSING_REQUIRED_VALUE, "The parameter '" + name + "' is required");
+		}
+	}
+
+	private void validateRequired(String name, Date value) throws ValidationException {
+		if(value == null){
+			throw new ValidationException(ErrorCode.MISSING_REQUIRED_VALUE, "The parameter '" + name + "' is required");
+		}
+	}
 
 }
