b145c1476a26360eee771fb4ba35042f0c69d750
Ravi Mishra <ravimishra.blr@gmail.com>
2015-06-12 16:16:47 -0500

    SeasonProgram service

--- src/main/java/com/ccighgo/service/rest/season/Seasons.java
+++ src/main/java/com/ccighgo/service/rest/season/Seasons.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ccighgo.service.components.season.SeasonServiceInterfaceImpl;
+import com.ccighgo.service.transport.season.beans.seasonprogram.SeasonProgram;
 import com.ccighgo.service.transport.seasons.beans.season.SeasonBean;
 import com.ccighgo.service.transport.seasons.beans.seasonslist.SeasonsList;
 
@@ -83,5 +84,12 @@ public class Seasons {
 	@Produces("application/json")
 	public void deleteSeason(@PathParam("id") String id) {
 	}
+	
+	@GET
+   @Path("program/season/{seasonId}")
+   @Produces("application/json")
+   public SeasonProgram getSeasonProgram(@PathParam("seasonId") String seasonId) {
+	   return seasonServices.getSeasonPrograms(seasonId);
+   }
 
 }
