591d68677246a1b1019e4c73cca533b80031c0ee
ahmedsamircpp2 <ahmed.amer.samir@gmail.com>
2015-07-16 16:26:53 -0500

    structure of background check

---
+++ src/main/java/com/ccighgo/service/components/backgroundcheck/BackgroundServiceImpl.java
@@ -0,0 +1,14 @@
+package com.ccighgo.service.components.backgroundcheck;
+
+import com.ccighgo.service.transport.seasons.beans.backgroundscreenrequest.ScreenRequest;
+import com.ccighgo.service.transport.seasons.beans.backgroundscreenresponse.ScreenResponse;
+
+public class BackgroundServiceImpl implements BackgroundServiceInterface {
+
+   @Override
+   public ScreenResponse requestScreen(ScreenRequest screenRequest) {
+
+      return null;
+   }
+
+}
