c75be9fe38d974910e4408df503de1557a150884
mahalingam <r.mahalingammsc@gmail.com>
2015-07-29 20:53:43 +0530

    usr mgmt error handling changes

--- src/main/java/com/ccighgo/service/transport/usermanagement/beans/cciuser/CCIUser.java
+++ src/main/java/com/ccighgo/service/transport/usermanagement/beans/cciuser/CCIUser.java
@@ -10,10 +10,13 @@ package com.ccighgo.service.transport.usermanagement.beans.cciuser;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+
+import com.ccighgo.service.transport.common.response.beans.Response;
 
 
 /**
@@ -61,7 +64,7 @@ import javax.xml.bind.annotation.XmlType;
     "userRole",
     "cciUserDepartmentPrograms"
 })
-public class CCIUser {
+public class CCIUser extends Response{
 
     protected int cciUserId;
     @XmlElement(required = true)
--- src/main/java/com/ccighgo/service/transport/usermanagement/beans/user/User.java
+++ src/main/java/com/ccighgo/service/transport/usermanagement/beans/user/User.java
@@ -10,10 +10,13 @@ package com.ccighgo.service.transport.usermanagement.beans.user;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+
+import com.ccighgo.service.transport.common.response.beans.Response;
 
 
 /**
@@ -79,7 +82,7 @@ import javax.xml.bind.annotation.XmlType;
     "permissions",
     "userNotes"
 })
-public class User {
+public class User extends Response{
 
     protected int cciUserId;
     @XmlElement(required = true)
