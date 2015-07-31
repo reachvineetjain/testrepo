82a493809ec0a48a102e8a003f6f1f61f68170c4
Rajan G <rajan.g@npcompete.org>
2015-07-31 16:29:01 +0530

    get country by id service completed

---
+++ src/main/java/com/ccighgo/service/components/errormessages/constants/UtilityServiceMessageConstants.java
@@ -0,0 +1,6 @@
+package com.ccighgo.service.components.errormessages.constants;
+
+public class UtilityServiceMessageConstants {
+
+   public static final String INVALID_COUNTRY_ID="invalid.country.id";   
+}
