2453c0ef8eff84b1195de3972197e18431d835f7
Ramadevi <ramadevi.t@npcompete.org>
2015-08-01 11:46:43 +0530

    Exception handling in season

---
+++ src/main/java/com/ccighgo/service/components/errormessages/constants/SeasonMessageConstants.java
@@ -0,0 +1,62 @@
+package com.ccighgo.service.components.errormessages.constants;
+
+public class SeasonMessageConstants {
+
+	public static final String SEASON_GET_ALL_SEASONS="error.get.all.seasons";
+	public static final String SEASON_NAME_ALREADY_EXIT="error.name.already.exist";
+	public static final String CREATE_SEASON="error.create.season";
+	public static final String VIEW_SEASON="error.to.view.season";
+	public static final String EDIT_SEASON="error.to.edit.season";
+	public static final String UPDATE_SEASON="error.update.season";
+	public static final String GET_SEASON_DEPT_HIGH_SCHOOL_PROGRAMS="error.get.dept.high.school.programs";
+	public static final String GET_SEASON_DEPT_WORK_PROGRAMS="error.get.dept.work.programs";
+	public static final String GET_SEASON_DEPT_GREEN_HEART_TRAVEL_PROGRAM="error.get.dept.green.heart.travel.program";
+	public static final String GET_SEASON_PROGRAM="error.get.season.program";
+	public static final String GET_SEASON_STATUS="error.get.season.status";
+	
+	public static final String GET_HSP_J1_HS_SEASON_DETAILS="error.get.HSP.J1.HS.Season.Details";
+	public static final String GET_J1_HS_BASIC_DETAIL="error.get.j1.hs.basic.details";
+	public static final String GET_J1_HS_JAN_START_DETAIL="error.get.j1.hs.jan.start";
+	public static final String UPDATE_J1_HS_JAN_START_DETAIL="error.update.j1.hs.jan.start";
+	public static final String GET_J1_HS_AUG_START_DETAIL="error.get.j1.hs.aug.start";
+	public static final String UPDATE_J1_HS_AUG_START_DETAIL="error.update.j1.hs.aug.start";
+	
+	public static final String GET_J1_HS_SEASON_FIELD_SETTINGS="error.get.j1.hs.season.field.settings";
+	public static final String UPDATE_J1_HS_SEASON_FIELD_SETTINGS="error.update.j1.hs.season.field.settings";
+	public static final String INVALID__J1_HS_FIELD_SETTINGS="error.invalid.j1.hs.field.settings";
+	
+	public static final String GET_HSP_J1_HS_PROGRAM_ALLOCATION="error.get.hsp.j1.hs.program.allocation";
+	public static final String UPDATE_HSP_J1_HS_PROGRAM_ALLOCATION="error.update.hsp.j1.hs.program.allocation";
+	//HSPF1ProgramAllocations
+	public static final String GET_HSP_F1_PROGRAM_ALLOCATION="error.get.hsp.f1.program.allocation";
+	public static final String UPDATE_HSP_F1_PROGRAM_ALLOCATION="error.update.hsp.f1.program.allocation";
+	public static final String GET_HSP_F1_FIELD_SETTINGS="error.get.hsp.f1.field.settings";
+	public static final String UPDATE_HSP_F1_FIELD_SETTINGS="error.update.hsp.f1.field.settings";
+	
+	public static final String GET_HSP_F1_AUGUST_1ST_SEMISTER="error.get.hsp.f1.august.1st.semester";
+	public static final String UPDATE_HSP_F1_AUGUST_1ST_SEMISTER="error.update.hsp.f1.august.1st.semester";
+	public static final String GET_HSP_F1_AUGUST_FULL_YEAR_DETAIL="error.get.hsp.f1.full.year.details";
+	public static final String UPDATE_HSP_F1_AUGUST_FULL_YEAR_DETAIL="error.update.hsp.f1.full.year.details";
+	public static final String GET_HSP_F1_JAN_SECOND_SEM_DETAIL="error.get.hsp.f1.jan.second.sem.details";
+	
+	public static final String UPDATE_HSP_F1_JAN_SECOND_SEM_DETAIL="error.update.hsp.f1.jan.second.sem.details";
+	public static final String UPDATE_HSP_F1_JAN_FULL_YEAR_START_DETAIL="error.update.hsp.f1.jan.full.year.details";
+	
+	public static final String GET_HSP_F1_JAN_FULL_YEAR_START_DETAIL="error.get.hsp.f1.jan.full.year.details";
+	public static final String GET_HSP_F1_DETAILS="error.get.hsp.f1.details";
+	public static final String GET_HSP_F1_NAME_STATUS="error.get.hsp.f1.name.status";
+	
+	public static final String GET_HSP_F1_SEASON_DETAILS="error.get.hsp.f1.season.details";
+	public static final String UPDATE_HSP_F1_SEASON_DETAILS="error.update.hsp.f1.details";
+	public static final String UPDATE_HSP_F1_DETAILS="error.update.hsp.f1.details";
+	public static final String UPDATE_HSP_F1_ACCOUNT_DETAILS="error.update.hsp.f1.account.details";
+	
+	public static final String UPDATE_HSP_J1_HS_SEASON_NAME_AND_STATUS="error.update.hsp.j1.hs.season.name.and.status";
+	public static final String INVALID_ID="error.invalid.id";
+	public static final String UPDATE_HSP_J1_HS_SEASON_DETAILS="error.update.hsp.j1.hs.season.details";
+	
+	
+	
+	
+	
+}
