39451114353a748fae8d331a28f817bff04ac427
Ravi Mishra <ravimishra.blr@gmail.com>
2015-08-07 09:56:03 -0500

    login management intermediate commit

--- src/main/java/com/ccighgo/service/components/login/LoginActionImpl.java
+++ src/main/java/com/ccighgo/service/components/login/LoginActionImpl.java
@@ -3,17 +3,44 @@
  */
 package com.ccighgo.service.components.login;
 
+import java.sql.Timestamp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ccighgo.db.entities.Login;
+import com.ccighgo.db.entities.LoginHistory;
+import com.ccighgo.jpa.repositories.LoginHistoryRepository;
+import com.ccighgo.jpa.repositories.LoginRepository;
 import com.ccighgo.service.auth.beans.Auth;
 
 /**
  * @author ravimishra
  *
  */
+@Component
 public class LoginActionImpl implements LoginAction {
 
-	@Override
-	public Auth login() {
-		return null;
-	}
+   @Autowired
+   LoginRepository loginRepository;
+   @Autowired
+   LoginHistoryRepository loginHistoryRepository;
+   @Autowired
+   UserManager userManager;
+
+   @Override
+   public Auth login() {
+      return null;
+   }
+   
+   @Transactional
+   private void updateHistory(String userName) {
+      LoginHistory history = new LoginHistory();
+      history.setLoggedOn(new Timestamp(System.currentTimeMillis()));
+      Login loggedInUser = loginRepository.findByLoginName(userName);
+      history.setLogin(loggedInUser);
+      loginHistoryRepository.save(history);
+   }
 
 }
---
+++ src/main/java/com/ccighgo/service/components/login/UserManager.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.ccighgo.service.components.login;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ravi
+ *
+ */
+@Component
+public class UserManager {
+
+}
