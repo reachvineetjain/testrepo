a2af614542604d2cc547f4957727951c82693fad
ahmedsamircpp2 <ahmed.amer.samir@gmail.com>
2015-08-07 18:10:30 -0500

    WSDL

--- src/main/java/com/ccighgo/service/components/wordpress/forms/PublishWordPressForms.java
+++ src/main/java/com/ccighgo/service/components/wordpress/forms/PublishWordPressForms.java
@@ -2,12 +2,9 @@ package com.ccighgo.service.components.wordpress.forms;
 
 import javax.xml.ws.Endpoint;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class PublishWordPressForms {
 
    static {
-      Endpoint.publish("http://localhost:9999/cci_gh_go/wordpressforms", new WordPressFormsImpl());
+      Endpoint.publish("http://localhost:9999/cci_gh_go_test/wordpressforms", new WordPressFormsImpl());
    }
 }
