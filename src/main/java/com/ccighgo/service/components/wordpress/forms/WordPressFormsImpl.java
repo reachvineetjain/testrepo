c205690aab1b761652820ab5cb1045466e11944d
ahmedsamircpp2 <ahmed.amer.samir@gmail.com>
2015-08-10 17:27:41 -0500

    Soap

--- src/main/java/com/ccighgo/service/components/wordpress/forms/WordPressFormsImpl.java
+++ src/main/java/com/ccighgo/service/components/wordpress/forms/WordPressFormsImpl.java
@@ -1,15 +1,22 @@
 package com.ccighgo.service.components.wordpress.forms;
 
-import javax.jws.WebService;
+import org.apache.log4j.Logger;
 
 import com.ccighgo.service.transport.integration.thirdparty.beans.internationalPartners.InternationalPartners;
 import com.ccighgo.utils.WordPressFormsConstants;
 
-@WebService(endpointInterface = "com.ccighgo.service.components.wordpress.forms.WordPressFormsInterface")
-public class WordPressFormsImpl implements WordPressFormsInterface {
+public class WordPressFormsImpl implements IWordPressForms {
+
+   public static final Logger LOGGER = Logger.getLogger(WordPressFormsImpl.class);
 
    @Override
-   public String inquiryPartner(InternationalPartners data) {
+   public String inquiryPartner(InternationalPartners inerInternationalPartners) {
+      LOGGER.info("Inquiry partner Is Called !!!");
+
+      if (inerInternationalPartners != null) {
+         LOGGER.info("Name " + inerInternationalPartners.getLegalBusinessName());
+      }
       return WordPressFormsConstants.DEFAULT_MESSAGE;
    }
+
 }
