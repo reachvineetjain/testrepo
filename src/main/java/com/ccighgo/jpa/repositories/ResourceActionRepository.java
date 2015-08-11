191b9fe9e8a754f19874238b105ee2072c73a27c
mahalingam <r.mahalingammsc@gmail.com>
2015-08-11 19:32:00 +0530

    user update services

--- src/main/java/com/ccighgo/jpa/repositories/ResourceActionRepository.java
+++ src/main/java/com/ccighgo/jpa/repositories/ResourceActionRepository.java
@@ -3,10 +3,14 @@
  */
 package com.ccighgo.jpa.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ccighgo.db.entities.ResourceAction;
+import com.ccighgo.db.entities.Season;
 
 /**
  * @author ravimishra
@@ -14,5 +18,8 @@ import com.ccighgo.db.entities.ResourceAction;
  */
 @Repository
 public interface ResourceActionRepository extends JpaRepository<ResourceAction, Integer> {
+   
+   @Query("select r from ResourceAction r")
+   List<ResourceAction> getAllResourceAction();
 
 }
