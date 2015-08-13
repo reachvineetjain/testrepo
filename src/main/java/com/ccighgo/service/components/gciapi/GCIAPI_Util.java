1e36368ea4f71fa161982a33710a9112d360476d
ahmedsamircpp2 <ahmed.amer.samir@gmail.com>
2015-08-13 17:59:39 -0500

    GCIAPI

--- src/main/java/com/ccighgo/service/components/gciapi/GCIAPI_Util.java
+++ src/main/java/com/ccighgo/service/components/gciapi/GCIAPI_Util.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.Charset;
 
 import org.apache.log4j.Logger;
 
@@ -15,8 +16,8 @@ import com.google.gson.Gson;
 public class GCIAPI_Util {
 
    private static final Logger LOGGER = Logger.getLogger(GCIAPI_Util.class);
-   public final static String API_BASE = "http://greenheartclub.org/q-api";
-   public final static String DN_ADD_USER = "NewUser";
+   public final static String API_BASE = "https://greenheartclub.org/q-api/";
+   public final static String DN_ADD_USER = "AddUser";
    public final static String DN_USER_EXISTS = "UserExists";
    public final static String DN_LOGIN_USER = "LoginUser";
    public final static String DN_SET_KEY = "SetKey";
@@ -27,21 +28,37 @@ public class GCIAPI_Util {
    public final static String DN_GET_ALL_VOLUNTEER_HOURS = "GetAllVolunteerHours";
 
    public static String executeURL(String serviceUrl) {
-      URL url;
       StringBuffer result = new StringBuffer();
+
+      String json = "";
       try {
-         url = new URL(serviceUrl);
+         URL url = new URL(serviceUrl);
+         // url = new URL(serviceUrl);
          URLConnection conn = url.openConnection();
-         BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+         conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+         String redirect = conn.getHeaderField("Location");
+         if (redirect != null) {
+            conn.connect();
+         }
+         BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), Charset.forName("UTF-8")));
          String line = "";
          while ((line = br.readLine()) != null) {
             result.append(line);
          }
          br.close();
+         // System.out.println(html);
+         json = result.toString();
+         // Pattern pattern = Pattern.compile("\"status\"(.*?)</pre>");
+         // Matcher m = pattern.matcher(html);
+         // if (m.find()) {
+         // json = m.group(1);
+         // json = "{\"status\"" + json;
+         // }
+         System.out.println(json);
       } catch (IOException e) {
          ExceptionUtil.logException(e, LOGGER);
       }
-      return result.toString();
+      return json;
    }
 
    public static GCIAPI_Response parseGCIAPIResult(String json) {
