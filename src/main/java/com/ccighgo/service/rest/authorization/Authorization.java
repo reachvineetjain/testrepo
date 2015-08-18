6b32dbd904cc2ffca2fcf32de9c2d631781bb079
Ravi Mishra <ravimishra.blr@gmail.com>
2015-08-18 16:26:09 -0500

    Login logout services

--- src/main/java/com/ccighgo/service/rest/authorization/Authorization.java
+++ src/main/java/com/ccighgo/service/rest/authorization/Authorization.java
@@ -3,10 +3,44 @@
  */
 package com.ccighgo.service.rest.authorization;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ccighgo.service.components.authorization.AuthorizationManagerInterface;
+import com.ccighgo.service.transport.usermanagement.beans.user.User;
+
 /**
  * @author ravi
  *
  */
+@Path("/authorize/")
+@Produces("application/json")
+@Consumes("application/json")
 public class Authorization {
+   
+   private static final Logger LOGGER = LoggerFactory.getLogger(Authorization.class); 
+   
+   @Autowired AuthorizationManagerInterface authorizationManager;
+   
+   /**
+    * RESTFul service gets user by id for edit
+    * 
+    * @param id
+    * @return User by id
+    */
+   @GET
+   @Path("cciusr/{userId}")
+   @Produces("application/json")
+   public User getCCIUserDetails(@PathParam("userId") String userId) {
+       return authorizationManager.getCCIUserDetails(userId);
+   }
+   
 
 }
