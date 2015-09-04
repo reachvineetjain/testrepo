3dbd7b4f4205e0fab058faf3e0a10ce2df912547
ahmedsamircpp2 <ahmed.amer.samir@gmail.com>
2015-09-04 13:52:21 -0500

    xsds for partner

--- src/main/java/com/ccighgo/service/transport/partner/beans/subpartner/package-info.java
+++ src/main/java/com/ccighgo/service/transport/partner/beans/subpartner/package-info.java
@@ -5,5 +5,5 @@
 // Generated on: 2015.09.03 at 04:58:39 PM CDT 
 //
 
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ccighgo.com/common")
-package com.ccighgo.service.transport.common.response.beans;
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ccighgo.com/subpartner")
+package com.ccighgo.service.transport.partner.beans.subpartner;
