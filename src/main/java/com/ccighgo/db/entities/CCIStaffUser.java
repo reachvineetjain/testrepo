d8dc9da2b50b73c6420ba1b1ffdb4a48e6943217
Rajan.g <rajan.gunasekaran@creospan.com>
2015-09-10 18:41:53 +0530

    done changes email activation for create cci user

--- src/main/java/com/ccighgo/db/entities/CCIStaffUser.java
+++ src/main/java/com/ccighgo/db/entities/CCIStaffUser.java
@@ -22,7 +22,7 @@ public class CCIStaffUser implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	/*@GeneratedValue(strategy=GenerationType.IDENTITY)*/
 	@Column(unique=true, nullable=false)
 	private int cciStaffUserId;
 
