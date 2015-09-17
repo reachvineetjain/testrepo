e7084088954e389b7036ab77c68dc715f12883e5
rajan-g <rajan.gunasekaran@creospan.com>
2015-09-17 11:20:21 +0530

    search partner user

--- src/main/java/com/ccighgo/jpa/repositories/PartnerUserRepository.java
+++ src/main/java/com/ccighgo/jpa/repositories/PartnerUserRepository.java
@@ -21,5 +21,8 @@ public interface PartnerUserRepository extends JpaRepository<PartnerUser, Intege
 
    @Query("SELECT p FROM PartnerUser p WHERE p.partner.partnerGoId=?1")
    List<PartnerUser> findByPartnerGoId(Integer partnerId);
+   
+   @Query("SELECT p FROM PartnerUser p WHERE p.firstName=%?1% or p.String=%?2% or p.email=%?3% or p.login.loginName=%?4% or p.active = ?5 ")
+   List<PartnerUser> searchPartnerUser(String firstName, String String , String email, String userName ,byte active);
 
 }
