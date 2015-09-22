30c704f0e3a8b26c690dd74e478feaa30c74d6ef
Rajan.g <rajan.gunasekaran@creospan.com>
2015-09-22 19:04:29 +0530

    created sub-partner repository

---
+++ src/main/java/com/ccighgo/service/transport/partner/beans/subpartner/SubPartnerTO.java
@@ -0,0 +1,20 @@
+package com.ccighgo.service.transport.partner.beans.subpartner;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+import com.ccighgo.service.transport.common.response.beans.Response;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "SubPartnerTO", propOrder = {
+      ""
+      
+})
+
+
+public class SubPartnerTO extends Response{
+   
+  
+
+}
