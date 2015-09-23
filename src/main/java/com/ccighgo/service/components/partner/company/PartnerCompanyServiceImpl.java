9411be84a8acae102ec17d1552ba942571f51fdd
Ravi Mishra <ravi.mishra@creospan.com>
2015-09-23 15:52:26 -0500

    Partner_Company_overview

--- src/main/java/com/ccighgo/service/components/partner/company/PartnerCompanyServiceImpl.java
+++ src/main/java/com/ccighgo/service/components/partner/company/PartnerCompanyServiceImpl.java
@@ -6,26 +6,28 @@ package com.ccighgo.service.components.partner.company;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import com.ccighgo.db.entities.Login;
 import com.ccighgo.db.entities.Partner;
+import com.ccighgo.db.entities.PartnerOffice;
 import com.ccighgo.db.entities.PartnerUser;
 import com.ccighgo.db.entities.Salutation;
 import com.ccighgo.exception.CcighgoException;
 import com.ccighgo.exception.ErrorCode;
-import com.ccighgo.jpa.repositories.CountryRepository;
-import com.ccighgo.jpa.repositories.PartnerOfficeRepository;
-import com.ccighgo.jpa.repositories.PartnerOfficeTypeRepository;
 import com.ccighgo.jpa.repositories.PartnerRepository;
-import com.ccighgo.jpa.repositories.PartnerStatusRepository;
-import com.ccighgo.jpa.repositories.PartnerUserRepository;
-import com.ccighgo.jpa.repositories.SalutationRepository;
-import com.ccighgo.jpa.repositories.StateRepository;
 import com.ccighgo.service.component.serviceutils.CommonComponentUtils;
 import com.ccighgo.service.component.serviceutils.MessageUtils;
+import com.ccighgo.service.components.errormessages.constants.PartnerCompanyDetailsMessageConstants;
 import com.ccighgo.service.components.errormessages.constants.PartnerSeasonMessageConstants;
+import com.ccighgo.service.transport.partner.beans.companydetail.PartnerAddress;
+import com.ccighgo.service.transport.partner.beans.companydetail.PartnerAddressCountry;
+import com.ccighgo.service.transport.partner.beans.companydetail.PartnerAddressState;
 import com.ccighgo.service.transport.partner.beans.companydetail.PartnerCompanyDetail;
 import com.ccighgo.service.transport.partner.beans.companydetail.PartnerCompanyDetails;
 import com.ccighgo.service.transport.partner.beans.companydetail.PartnerCompanyStatus;
+import com.ccighgo.service.transport.partner.beans.companydetail.PartnerMailingAddress;
+import com.ccighgo.service.transport.partner.beans.companydetail.PartnerPhysicalAddress;
 import com.ccighgo.service.transport.partner.beans.companydetail.PartnerPrimaryContact;
 import com.ccighgo.service.transport.partner.beans.companydetail.PrimaryContactSalutation;
 import com.ccighgo.utils.CCIConstants;
@@ -34,21 +36,14 @@ import com.ccighgo.utils.CCIConstants;
  * @author ravi
  *
  */
+@Component
 public class PartnerCompanyServiceImpl implements PartnerCompanyService {
 
    private static final Logger LOGGER = LoggerFactory.getLogger(PartnerCompanyServiceImpl.class);
 
    @Autowired MessageUtils messageUtil;
-   @Autowired StateRepository stateRepository;
-   @Autowired CountryRepository countryRepository;
-   @Autowired SalutationRepository salutationRepository;
    @Autowired CommonComponentUtils componentUtils;
-
    @Autowired PartnerRepository partnerRepository;
-   @Autowired PartnerUserRepository partnerUserRepository;
-   @Autowired PartnerStatusRepository partnerStatusRepository;
-   @Autowired PartnerOfficeRepository partnerOfficeRepository;
-   @Autowired PartnerOfficeTypeRepository partnerOfficeTypeRepository;
 
    @Override
    public PartnerCompanyDetail getPartnerCompanyDetails(String partnerGoId) {
@@ -79,28 +74,93 @@ public class PartnerCompanyServiceImpl implements PartnerCompanyService {
          partnerCompanyDetails.setSubscribeCCINewsletter(partner.getSubscribeToCCINewsletter() == CCIConstants.ACTIVE ? true : false);
          partnerCompanyDetails.setRecieveHSPNotificationEmails(partner.getReceiveAYPMails() == CCIConstants.ACTIVE ? true : false);
          partnerCompanyDetails.setGeneralEmail(partner.getEmail());
-         partnerCompanyDetails.setUserName(partner.getGoIdSequence().getLogin().get(0).getLoginName());
-         partnerCompanyDetail.setPartnerCompanyDetails(partnerCompanyDetails);
-         
-         PartnerPrimaryContact partnerPrimaryContact = new PartnerPrimaryContact();
-         PrimaryContactSalutation primaryContactSalutation = new PrimaryContactSalutation();
-         Salutation salutation = null;
-         PartnerUser partnerUser = null;
-         for(PartnerUser puser:partner.getPartnerUsers()){
-            if(partner.getPartnerGoId()==puser.getPartner().getPartnerGoId()){
-               salutation=puser.getSalutation();
-               partnerUser=puser;
+         Login partnerLogin = null;
+         for (Login login : partner.getGoIdSequence().getLogin()) {
+            if (partner.getPartnerGoId() == login.getGoIdSequence().getGoId()) {
+               partnerLogin=login;
+               break;
             }
          }
-         primaryContactSalutation.setSalutationId(salutation.getSalutationId());
-         primaryContactSalutation.setSalutation(salutation.getSalutationName());
-         partnerPrimaryContact.setPrimaryContactSalutation(primaryContactSalutation);
-         partnerPrimaryContact.setPrimaryContactTitle(partnerUser.getTitle());
+         if(partnerLogin!=null){
+            partnerCompanyDetails.setUserName(partnerLogin.getLoginName());
+         }
+         partnerCompanyDetail.setPartnerCompanyDetails(partnerCompanyDetails);
+         PartnerPrimaryContact partnerPrimaryContact = new PartnerPrimaryContact();
 
+         Salutation salutation = null;
+         PartnerUser partnerUser = null;
+         PartnerOffice partnerOffice = null;
+         for (PartnerUser puser : partner.getPartnerUsers()) {
+            if (partner.getPartnerGoId() == puser.getPartner().getPartnerGoId()) {
+               salutation = puser.getSalutation();
+               partnerUser = puser;
+               break;
+            }
+         }
+         for (PartnerOffice po : partner.getPartnerOffices()) {
+            if (partner.getPartnerGoId() == po.getPartner().getPartnerGoId()) {
+               partnerOffice = po;
+               break;
+            }
+         }
+         if (salutation != null) {
+            PrimaryContactSalutation primaryContactSalutation = new PrimaryContactSalutation();
+            primaryContactSalutation.setSalutationId(salutation.getSalutationId());
+            primaryContactSalutation.setSalutation(salutation.getSalutationName());
+            partnerPrimaryContact.setPrimaryContactSalutation(primaryContactSalutation);
+         }
+         if (partnerUser != null) {
+            partnerPrimaryContact.setPrimaryContactTitle(partnerUser.getTitle());
+            partnerPrimaryContact.setPrimaryContactFirstName(partnerUser.getFirstName());
+            partnerPrimaryContact.setPrimaryContactLastName(partnerUser.getLastName() != null ? partnerUser.getLastName() : null);
+            partnerPrimaryContact.setPrimaryContactEmail(partnerUser.getEmail() != null ? partnerUser.getEmail() : null);
+            partnerPrimaryContact.setPrimaryContactPhone(partnerUser.getPhone() != null ? partnerUser.getPhone() : null);
+            partnerPrimaryContact.setPrimaryContactEmergencyPhone(partnerUser.getEmergencyPhone() != null ? partnerUser.getEmergencyPhone() : null);
+            partnerPrimaryContact.setPrimaryContactFax(partnerUser.getFax() != null ? partnerUser.getFax() : null);
+            partnerPrimaryContact.setPrimaryContactShouldRecieveCCINotification(false/* "TODO:field not available now" */);
+            partnerPrimaryContact.setPrimaryContactSkypeId(partnerUser.getSkypeId() != null ? partnerUser.getSkypeId() : null);
+            partnerPrimaryContact.setPrimaryContactWebsite(partnerOffice.getWebsite() != null ? partnerOffice.getWebsite() : null);
+            partnerCompanyDetail.setPartnerPrimaryContact(partnerPrimaryContact);
+         }
+         PartnerAddress physicalAddress = new PartnerAddress();
+         
+         PartnerAddressState physicalAddressState = new PartnerAddressState();
+         PartnerAddressCountry physicalAddressCountry = new PartnerAddressCountry();
+         physicalAddress.setAddressLineOne(partner.getPhysicalAddressLineOne()!=null?partner.getPhysicalAddressLineOne():null);
+         physicalAddress.setAddressLineTwo(partner.getPhysicalAddressLineTwo()!=null?partner.getPhysicalAddressLineTwo():null);
+         physicalAddress.setCity(partner.getPhysicalCity()!=null?partner.getPhysicalCity():null);
+         physicalAddress.setZipCode(partner.getPhysicalZipcode()!=null?partner.getPhysicalZipcode():null);
+         physicalAddress.setPartnerAddressState(physicalAddressState);
+         physicalAddress.setPartnerAddressCountry(physicalAddressCountry);
+         
+         PartnerAddress mailingAddress = new PartnerAddress();
+         PartnerAddressState mailingAddressState = new PartnerAddressState();
+         PartnerAddressCountry mailingAddressCountry = new PartnerAddressCountry();
+         mailingAddress.setAddressLineOne(partner.getAddressLineOne()!=null?partner.getAddressLineOne():null);
+         mailingAddress.setAddressLineTwo(partner.getAddressLineTwo()!=null?partner.getAddressLineTwo():null);
+         mailingAddress.setCity(partner.getCity()!=null?partner.getCity():null);
+         mailingAddress.setZipCode(partner.getZipcode()!=null?partner.getZipcode():null);
+         mailingAddress.setPartnerAddressState(mailingAddressState);
+         mailingAddress.setPartnerAddressCountry(mailingAddressCountry);
+         PartnerMailingAddress partnerMailingAddress = new PartnerMailingAddress();
+         partnerMailingAddress.setPartnerMailingAddress(mailingAddress);
+         
+         PartnerPhysicalAddress partnerPhysicalAddress = new PartnerPhysicalAddress();
+         partnerPhysicalAddress.setPartnerPhysicalAddress(physicalAddress);
+         partnerCompanyDetail.setPartnerPhysicalAddress(partnerPhysicalAddress);
+         if(partner.getMailingAddressIsSameAsPhysicalAdress()==CCIConstants.ACTIVE){
+            partnerCompanyDetail.setPartnerMailingAddressSame(true);
+            partnerCompanyDetail.setPartnerMailingAddress(null);
+         }
+         else{
+            partnerCompanyDetail.setPartnerMailingAddress(partnerMailingAddress);
+         }
+         partnerCompanyDetail.setStatus(componentUtils.getStatus(CCIConstants.SUCCESS, CCIConstants.TYPE_INFO, ErrorCode.REGION_SERVICE_CODE.getValue(),
+               messageUtil.getMessage(CCIConstants.SERVICE_SUCCESS)));
       } catch (CcighgoException e) {
-         partnerCompanyDetail.setStatus(componentUtils.getStatus(CCIConstants.FAILURE, CCIConstants.TYPE_ERROR, ErrorCode.ERROR_GET_PARTNER_SEASON.getValue(),
-               messageUtil.getMessage(PartnerSeasonMessageConstants.ERROR_GET_PARTNER_SEASON_LIST)));
-         LOGGER.error(messageUtil.getMessage(PartnerSeasonMessageConstants.ERROR_GET_PARTNER_SEASON_LIST), e);
+         partnerCompanyDetail.setStatus(componentUtils.getStatus(CCIConstants.FAILURE, CCIConstants.TYPE_ERROR, ErrorCode.ERROR_GET_PARTNER_COMPANY_DETAIL.getValue(),
+               messageUtil.getMessage(PartnerCompanyDetailsMessageConstants.ERROR_GET_PARTNER_COMPANY_DETAIL)));
+         LOGGER.error(messageUtil.getMessage(PartnerCompanyDetailsMessageConstants.ERROR_GET_PARTNER_COMPANY_DETAIL), e);
       }
       return partnerCompanyDetail;
    }
