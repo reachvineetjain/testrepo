7c927c8bc45f8df6f8a5930817b6a39f73943d16
ahmedsamircpp2 <ahmed.amer.samir@gmail.com>
2015-10-15 17:11:44 -0500

    partner admin xsd , partner inquiry email validations service

--- src/main/java/com/ccighgo/jpa/repositories/AdminWorkQueueCategoryRepository.java
+++ src/main/java/com/ccighgo/jpa/repositories/AdminWorkQueueCategoryRepository.java
@@ -6,6 +6,7 @@ package com.ccighgo.jpa.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ccighgo.db.entities.AdminWorkQueueCategory;
@@ -17,6 +18,7 @@ import com.ccighgo.db.entities.AdminWorkQueueCategory;
 @Repository
 public interface AdminWorkQueueCategoryRepository extends JpaRepository<AdminWorkQueueCategory, Integer> {
 
+   @Query("SELECT c FROM AdminWorkQueueCategory c where c.adminWorkQueueType.adminWQTypeId = ?1")
    List<AdminWorkQueueCategory> findAllCategoriesByTypeId(int adminWorkQueueTypeId);
       
 }
--- src/main/java/com/ccighgo/jpa/repositories/PartnerAgentInquiryRepository.java
+++ src/main/java/com/ccighgo/jpa/repositories/PartnerAgentInquiryRepository.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Repository;
 import com.ccighgo.db.entities.PartnerAgentInquiry;
 @Repository
 public interface PartnerAgentInquiryRepository extends JpaRepository<PartnerAgentInquiry, Integer> {
-   @Query("SELECT p FROM PartnerAgentInquiry p WHERE p.website =?1")
-   PartnerAgentInquiry findByWebSite(String website);
+   @Query("SELECT p FROM PartnerAgentInquiry p WHERE p.website =?1 OR p.website=?2")
+   PartnerAgentInquiry findByWebSite(String website, String secondFormat);
    @Query("SELECT p FROM PartnerAgentInquiry p WHERE p.businessName =?1")
    PartnerAgentInquiry findByLegalName(String website);
    @Query("SELECT p FROM PartnerAgentInquiry p WHERE p.partner.partnerGoId =?1")
