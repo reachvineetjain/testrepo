1f71b5f0758e0f962c5794779ab7e9e629958335
Ravi Mishra <ravi.mishra@creospan.com>
2015-10-23 17:22:59 -0500

    Partner season admin list

--- src/main/java/com/ccighgo/service/transport/partner/beans/partner/admin/season/package-info.java
+++ src/main/java/com/ccighgo/service/transport/partner/beans/partner/admin/season/package-info.java
@@ -5,5 +5,5 @@
 // Generated on: 2015.10.23 at 03:44:41 PM CDT 
 //
 
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ccighgo.com/common")
-package com.ccighgo.service.transport.common.response.beans;
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ccighgo.com/partneradminseason")
+package com.ccighgo.service.transport.partner.beans.partner.admin.season;
