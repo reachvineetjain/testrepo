51b4ae0c432d978c485dd27178621e191199fe1c
Ravi Mishra <ravi.mishra@creospan.com>
2015-10-26 13:48:01 -0500

    update services

--- src/main/java/com/ccighgo/service/transport/partner/beans/partner/season/admin/application/package-info.java
+++ src/main/java/com/ccighgo/service/transport/partner/beans/partner/season/admin/application/package-info.java
@@ -5,5 +5,5 @@
 // Generated on: 2015.10.26 at 10:07:49 AM CDT 
 //
 
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ccighgo.com/common")
-package com.ccighgo.service.transport.common.response.beans;
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ccighgo.com/seasonadminapplication")
+package com.ccighgo.service.transport.partner.beans.partner.season.admin.application;
