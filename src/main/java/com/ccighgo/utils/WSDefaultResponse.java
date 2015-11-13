190c93a9b45b895883989a9df3a57eb609225e20
ahmedsamircpp2 <ahmed.amer.samir@gmail.com>
2015-11-13 12:52:48 -0600

    add created by as result of add new note

--- src/main/java/com/ccighgo/utils/WSDefaultResponse.java
+++ src/main/java/com/ccighgo/utils/WSDefaultResponse.java
@@ -1,18 +1,28 @@
 package com.ccighgo.utils;
 
 import com.ccighgo.service.transport.common.response.beans.Response;
+import com.ccighgo.service.transport.partner.beans.generic.topic.NoteUserCreator;
 
 public class WSDefaultResponse extends Response {
 
-   private int topicOfNotesId;
+   protected NoteUserCreator createdBy;
+   private Integer topicOfNotesId;
 
-   public int getTopicOfNotesId() {
+   
+   public NoteUserCreator getCreatedBy() {
+      return createdBy;
+   }
+
+   public void setCreatedBy(NoteUserCreator createdBy) {
+      this.createdBy = createdBy;
+   }
+
+   public Integer getTopicOfNotesId() {
       return topicOfNotesId;
    }
 
-   public void setTopicOfNotesId(int topicOfNotesId) {
+   public void setTopicOfNotesId(Integer topicOfNotesId) {
       this.topicOfNotesId = topicOfNotesId;
    }
-   
-   
+
 }
