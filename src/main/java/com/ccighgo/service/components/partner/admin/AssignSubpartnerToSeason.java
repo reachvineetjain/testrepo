7c3f80f236f0a5a0007a3a511b971fd3e8259740
ahmedsamircpp2 <ahmed.amer.samir@gmail.com>
2015-12-11 12:51:30 -0600

    assign seasons to partner

--- src/main/java/com/ccighgo/service/components/partner/admin/AssignSubpartnerToSeason.java
+++ src/main/java/com/ccighgo/service/components/partner/admin/AssignSubpartnerToSeason.java
@@ -4,15 +4,14 @@ import java.util.List;
 
 public class AssignSubpartnerToSeason {
 
-   private List<Integer> seasonId;
+   private List<AssignedSeasonData> assignedSeasonData;
    private Integer subPartner;
-   private Integer departmentProgramId;
    private Integer loginId;
-   public List<Integer> getSeasonId() {
-      return seasonId;
+   public List<AssignedSeasonData> getAssignedSeasonData() {
+      return assignedSeasonData;
    }
-   public void setSeasonId(List<Integer> seasonId) {
-      this.seasonId = seasonId;
+   public void setAssignedSeasonData(List<AssignedSeasonData> assignedSeasonData) {
+      this.assignedSeasonData = assignedSeasonData;
    }
    public Integer getSubPartner() {
       return subPartner;
@@ -20,18 +19,12 @@ public class AssignSubpartnerToSeason {
    public void setSubPartner(Integer subPartner) {
       this.subPartner = subPartner;
    }
-   public Integer getDepartmentProgramId() {
-      return departmentProgramId;
-   }
-   public void setDepartmentProgramId(Integer departmentProgramId) {
-      this.departmentProgramId = departmentProgramId;
-   }
    public Integer getLoginId() {
       return loginId;
    }
    public void setLoginId(Integer loginId) {
       this.loginId = loginId;
    }
-
    
+
 }
