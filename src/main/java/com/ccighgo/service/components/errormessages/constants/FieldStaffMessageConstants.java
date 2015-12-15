c46d09602f8e12b31c6b0b2f17c6321704f8f5a7
Ravi Mishra <ravi.mishra@creospan.com>
2015-12-15 11:00:55 -0600

    error code update for fsl season listing

--- src/main/java/com/ccighgo/service/components/errormessages/constants/FieldStaffMessageConstants.java
+++ src/main/java/com/ccighgo/service/components/errormessages/constants/FieldStaffMessageConstants.java
@@ -5,4 +5,11 @@ public class FieldStaffMessageConstants {
    public static final String INVALID_FIELDSTAFF_ROLL="invalid.fieldstaff.roll";
    public static final String ERROR_GETTING_FIELDSTAFF_BY_ROLL="error.getting.fieldstaff.by.roll";
    public static final String ERROR_GETTING_FIELDSTAFF_DETAIL="error.getting.fieldstaff.detail";
+   
+   //Field staff season listing
+   public static final String INVALID_FSLSEASONID="invalid.fieldstaff.leadership.season.id";
+   public static final String INVALID_SEASONID="invalid.fs.season.id";
+   public static final String INVALID_DEPT_PRG_ID="invalid.fs.dept.prg.id";
+   public static final String INVALID_SIGNNED_CONTRACT_VALUE="invalid.fs.season.signed.contract.value";
+   public static final String NO_FSL_RECORD_FOUND_TO_UPDATE="no.fsl.season.found";
 }
