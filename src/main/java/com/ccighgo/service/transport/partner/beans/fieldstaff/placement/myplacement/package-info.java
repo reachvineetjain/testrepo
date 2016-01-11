889380b15d553e219fdf144bddadb3091e7fc5b6
sinshawDemisse <sinshaw.demisse@gmail.com>
2016-01-11 15:39:22 -0600

    fieldstaff participant, erd dashboard and placements

--- src/main/java/com/ccighgo/service/transport/partner/beans/fieldstaff/placement/myplacement/package-info.java
+++ src/main/java/com/ccighgo/service/transport/partner/beans/fieldstaff/placement/myplacement/package-info.java
@@ -5,5 +5,5 @@
 // Generated on: 2016.01.11 at 02:31:13 PM CST 
 //
 
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ccighgo.com/common")
-package com.ccighgo.service.transport.common.response.beans;
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ccighgo.com/fieldstaffdashboard")
+package com.ccighgo.service.transport.partner.beans.fieldstaff.placement.myplacement;
