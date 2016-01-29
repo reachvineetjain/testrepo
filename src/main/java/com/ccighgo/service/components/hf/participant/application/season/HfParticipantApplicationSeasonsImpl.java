658da8a331871ca2d8fee0ca450f82f2978bbe72
Vijay Kadam <vijay.kadam@creospan.com>
2016-01-29 12:36:46 +0530

    Service implementation for HF seasons list

--- src/main/java/com/ccighgo/service/components/hf/participant/application/season/HfParticipantApplicationSeasonsImpl.java
+++ src/main/java/com/ccighgo/service/components/hf/participant/application/season/HfParticipantApplicationSeasonsImpl.java
@@ -30,7 +30,7 @@ public class HfParticipantApplicationSeasonsImpl implements HfParticipantApplica
 
    private static final Logger LOGGER = Logger.getLogger(HfParticipantApplicationSeasonsImpl.class);
 
-   private static final String SP_HF_SEASON_LIST = "CALL SPAdminHostFamilyAddedSeason(?)";
+   private static final String SP_HF_SEASON_LIST = "CALL SPHostFamilySeasonList(?)";
    private static final String SPACE = " ";
 
    @Autowired MessageUtils messageUtil;
@@ -59,9 +59,10 @@ public class HfParticipantApplicationSeasonsImpl implements HfParticipantApplica
          List<Object[]> results = query.getResultList();
 
          /*
-          * The list of the fields in the result set. 0.seasonName ,
-          * 1.programSeasonStatus, 2.RD Name 3.RM Name, 4.AC Name, 5.LC Name,
-          * 6.HF season status,
+          * The list of the fields in the result set fetched by call to SP.
+          * 0.seasonName ,1.programSeasonStatus 2.RDFirstName 3.RDLastName
+          * 4.RMFirstName,5.RMLastName,6.ACFirstName,
+          * 7.ACLastName,8.LCFirstName,9.LCLastName,10.HFSeasonStatus
           */
          if (results != null && results.size() > 0) {
             for (Object[] obj : results) {
@@ -70,9 +71,15 @@ public class HfParticipantApplicationSeasonsImpl implements HfParticipantApplica
                season.setGoId(Integer.valueOf(goId));
                season.setSeasonName(obj[0] != null ? obj[0].toString() : SPACE);
                season.setProgramSeasonStatus(obj[1] != null ? obj[1].toString() : SPACE);
-               season.setLcName(obj[2] != null ? obj[2].toString() : SPACE);
-               season.setRdName(obj[3] != null ? obj[3].toString() : SPACE);
-               season.setHfSeasonStatus(obj[4] != null ? obj[4].toString() : SPACE);
+               season.setRdFirstName(obj[2] != null ? obj[2].toString() : SPACE);
+               season.setRdLastName(obj[3] != null ? obj[3].toString() : SPACE);
+               season.setRmFirstName(obj[4] != null ? obj[4].toString() : SPACE);
+               season.setRmLastName(obj[5] != null ? obj[5].toString() : SPACE);
+               season.setAcFirstName(obj[6] != null ? obj[6].toString() : SPACE);
+               season.setAcLastName(obj[7] != null ? obj[7].toString() : SPACE);
+               season.setLcFirstName(obj[8] != null ? obj[8].toString() : SPACE);
+               season.setLcLastName(obj[9] != null ? obj[9].toString() : SPACE);
+               season.setHfSeasonStatus(obj[10] != null ? obj[10].toString() : SPACE);
                hfParticipantSeasonList.getHfParticipantSeasons().add(season);
 
             }
