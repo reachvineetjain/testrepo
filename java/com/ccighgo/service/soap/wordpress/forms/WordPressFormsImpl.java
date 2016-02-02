d181ef3c886780e42baaa8ff2bfa5b76f9888442
Ahmed Samir Abdelmaaboud <ahmed.amer.samir@gmail.com>
2016-02-02 09:38:23 -0600

    fix add new contact

--- java/com/ccighgo/service/soap/wordpress/forms/WordPressFormsImpl.java
+++ java/com/ccighgo/service/soap/wordpress/forms/WordPressFormsImpl.java
@@ -342,6 +342,8 @@ public class WordPressFormsImpl implements IWordPressForms {
 					System.out.println(message);
 					return message;
 				}
+				GoIdSequence goIdSequence = new GoIdSequence();
+				goIdSequence = goIdSequenceRepository.saveAndFlush(goIdSequence);
 				
 				pa =new HostFamilyInquiry();
 				pa.setAddress(HostFamilyData.getAddress());
@@ -355,8 +357,6 @@ public class WordPressFormsImpl implements IWordPressForms {
 				pa.setPreferredPhoneNumber(HostFamilyData.getPreferredPhone());
 				pa.setOptionalPhoneNumber(HostFamilyData.getOptionalPhone());
 				
-				GoIdSequence goIdSequence = new GoIdSequence();
-				goIdSequence = goIdSequenceRepository.save(goIdSequence);
 				System.out.println("GoID : " +goIdSequence.getGoId());
 				pa.setHostFamilyInquiryId(goIdSequence.getGoId());
 				hostFamilyInquiryRepository.saveAndFlush(pa);
