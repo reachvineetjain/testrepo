1dab4ef4bc128872c3b8ca3177b7d2d8469081e8
Ahmed Samir Abdelmaaboud <ahmed.amer.samir@gmail.com>
2016-03-08 17:44:34 -0600

    list airports, delete airport,pet and member in hostfamily

---
+++ src/main/java/com/ccighgo/service/components/hf/participant/application/process/util/HFAirportData.java
@@ -0,0 +1,44 @@
+package com.ccighgo.service.components.hf.participant.application.process.util;
+
+public class HFAirportData {
+   public String airportCity;
+   public String airportCode;
+   public String airportName;
+   public boolean isInternational;
+
+   public HFAirportData() {
+   }
+
+   public String getAirportCity() {
+      return airportCity;
+   }
+
+   public void setAirportCity(String airportCity) {
+      this.airportCity = airportCity;
+   }
+
+   public String getAirportCode() {
+      return airportCode;
+   }
+
+   public void setAirportCode(String airportCode) {
+      this.airportCode = airportCode;
+   }
+
+   public String getAirportName() {
+      return airportName;
+   }
+
+   public void setAirportName(String airportName) {
+      this.airportName = airportName;
+   }
+
+   public boolean isInternational() {
+      return isInternational;
+   }
+
+   public void setInternational(boolean isInternational) {
+      this.isInternational = isInternational;
+   }
+
+}
