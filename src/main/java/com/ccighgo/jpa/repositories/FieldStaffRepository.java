e726fa4185369a40a27bb441cfe19444caa4d3a4
Vijay Kadam <vijay.kadam@creospan.com>
2016-04-11 01:42:06 +0530

    Region Assignment-Service changes to show the value in the Assigned To field in the assignment popup window to 'Not Assigned' only when both region area and state code are null

--- src/main/java/com/ccighgo/jpa/repositories/FieldStaffRepository.java
+++ src/main/java/com/ccighgo/jpa/repositories/FieldStaffRepository.java
@@ -17,8 +17,8 @@ public interface FieldStaffRepository extends JpaRepository<FieldStaff, Integer>
    @Query("select fs from FieldStaff fs where fs.fieldStaffType.fieldStaffTypeId = ?1")
    List<FieldStaff> findAllRDStaff(Integer fieldstafftypecoderd);
 
-   @Query("select fs from FieldStaff fs where fs.fieldStaffType.fieldStaffTypeId in (2,3,4)")
-   List<FieldStaff> findAllStaffRatherERDorRD();
+   @Query("select fs from FieldStaff fs where fs.fieldStaffType.fieldStaffTypeId in (1,2,3,4)")
+   List<FieldStaff> findAllStaffRatherERD();
 
    @Query("select fs from FieldStaff fs where fs.fieldStaffType.fieldStaffTypeCode = ?1")
    List<FieldStaff> findAllByFieldStaffType(String fieldStaffTypeCode);
