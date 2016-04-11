8774426a40c3cf7fa353fbb4783d7962bcec23e1
priyanka9090 <priyanka.singh@creospan.com>
2016-04-11 15:29:23 +0530

    RegionAssignment-Add validation  for null values of LookupUsstate

--- src/main/java/com/ccighgo/jpa/repositories/FieldStaffRepository.java
+++ src/main/java/com/ccighgo/jpa/repositories/FieldStaffRepository.java
@@ -17,7 +17,7 @@ public interface FieldStaffRepository extends JpaRepository<FieldStaff, Integer>
    @Query("select fs from FieldStaff fs where fs.fieldStaffType.fieldStaffTypeId = ?1")
    List<FieldStaff> findAllRDStaff(Integer fieldstafftypecoderd);
 
-   @Query("select fs from FieldStaff fs where fs.fieldStaffType.fieldStaffTypeId in (1,2,3,4)")
+   @Query("select fs from FieldStaff fs where fs.fieldStaffType.fieldStaffTypeId in (2,3,4)")
    List<FieldStaff> findAllStaffRatherERD();
 
    @Query("select fs from FieldStaff fs where fs.fieldStaffType.fieldStaffTypeCode = ?1")
