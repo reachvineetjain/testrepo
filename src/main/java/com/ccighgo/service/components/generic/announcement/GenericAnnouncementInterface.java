3bc9c660215b28f0afd197faf9677615c243d6c9
Ahmed Samir Amer <Ahmed@Ahmeds-MacBook.local>
2016-05-24 15:42:19 -0500

    HF modification, bug1597

---
+++ src/main/java/com/ccighgo/service/components/generic/announcement/GenericAnnouncementInterface.java
@@ -0,0 +1,16 @@
+package com.ccighgo.service.components.generic.announcement;
+
+import com.ccighgo.service.transport.partner.beans.genericannouncement.announcements.GenericAnnoucement;
+import com.ccighgo.service.transport.partner.beans.genericquicklink.quicklinks.GenericQuickLinks;
+
+/**
+ * 
+ * @author Ahmed
+ *
+ */
+public interface GenericAnnouncementInterface {
+
+	GenericAnnoucement fetchAnnouncement();
+
+  
+  }
